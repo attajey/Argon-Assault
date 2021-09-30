d0d06d20f5eb96ed3ecff3f62200ec79838d20ae
Atta Jirofty <jirofty76@gmail.com>
2021-09-30 12:38:22 +0330

    Assets Folder Organized

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,21 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MusicPlayer : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
     // Start is called before the first frame update
     void Start()
     {
         Invoke("LoadFirstScene", 2f);
     }
-    void LoadFirstScene() 
+    void LoadFirstScene()
     {
         SceneManager.LoadScene(1);
     }
 
- 
 }
