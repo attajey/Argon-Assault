10ffb76feefb9922f00c2ce8027eb376c57fc993
Atta Jirofty <jirofty76@gmail.com>
2021-10-01 10:31:47 +0330

    Explosion Particle System Added
    Now on collision, Explosion effect will be appeared, player controllers will be disabled & player will jump to level 1.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,17 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; //ok as long as this is the only script that load scenes
 
 public class CollisionHandler : MonoBehaviour
 {
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("FX prefab on player")] [SerializeField] GameObject deathFX;
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", levelLoadDelay);
     }
 
     private void StartDeathSequence()
     {
-        print("Player Dying");
         SendMessage("OnPlayerDeath");
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
     }
 }
