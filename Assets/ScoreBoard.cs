c5561ebd2daf64c8c2593fcf188c2c09dcf8b103
Atta Jirofty <jirofty76@gmail.com>
2021-10-01 14:34:44 +0330

    Score Board Added
    Now you can see your score. it will be incremented by shooting enemy ships. Each enemy ship can have a different score point. You can modify it in Inspector by altering "Score Per Hit" value of each enemy ship.

---
+++ Assets/ScoreBoard.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    int score;
+    Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+
+        
+    }
+    public void ScoreHit(int score)
+    {
+        this.score += score;
+        scoreText.text = this.score.ToString();
+
+    }
+
+}
